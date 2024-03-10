a561341753e0f1423f57603eefa2c71cb23fa5c9
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-03-10 19:50:37 +0530

    Added Git and Github Certificate

--- js/script.js
+++ js/script.js
@@ -42,20 +42,7 @@ window.onscroll = () => {
 };
 
 // Scroll Revel
-ScrollReveal({
-  // reset: true,
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
-
-ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box, .contact",
-  { origin: "bottom" }
-);
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
-ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+ 
 
 // Typed JS
 const typed = new Typed(".multiple-text", {
